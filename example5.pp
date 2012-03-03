ed3143f69183f57ab8ea0eaacffead7cb4446313
Eric Gerlach <eric+git@gerlach.ca>
2012-03-03 12:06:02 -0500

    Turn what we have been doing into a class

--- example5.pp
+++ example5.pp
@@ -1,28 +1,30 @@
-$motd = "Welcome to ${::fqdn}, my hell"
+class ngircd {
+	$motd = "Welcome to ${::fqdn}, my hell"
 
-package { "ngircd":
-	provider => "apt",
-	ensure => present
-}
+	package { "ngircd":
+		provider => "apt",
+		ensure => present
+	}
 
-service { "ngircd":
-	ensure => running,
-	hasrestart => true,
-	hasstatus => true,
-	require => Package[ngircd]
-}
+	service { "ngircd":
+		ensure => running,
+		hasrestart => true,
+		hasstatus => true,
+		require => Package[ngircd]
+	}
 
-file { "/etc/ngircd/ngircd.conf":
-	ensure => present,
-	owner => irc,
-	group => irc,
-	mode => 0644,
-	source => "/home/egerlach/puppet/ngircd.conf",
-	notify => Service[ngircd]
-}
+	file { "/etc/ngircd/ngircd.conf":
+		ensure => present,
+		owner => irc,
+		group => irc,
+		mode => 0644,
+		source => "/home/egerlach/puppet/ngircd.conf",
+		notify => Service[ngircd]
+	}
 
-file { "/etc/ngircd/ngircd.motd":
-	ensure => present,
-	content => $motd,
-	notify => Service[ngircd]
+	file { "/etc/ngircd/ngircd.motd":
+		ensure => present,
+		content => $motd,
+		notify => Service[ngircd]
+	}
 }
