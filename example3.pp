55abd888e8ccf0665797f8bdfcb59448aac12024
Eric Gerlach <eric+git@gerlach.ca>
2012-03-03 11:47:36 -0500

    Manage file and notify service on change

--- example3.pp
+++ example3.pp
@@ -9,3 +9,12 @@ service { "ngircd":
 	hasstatus => true,
 	require => Package[ngircd]
 }
+
+file { "/etc/ngircd/ngircd.conf":
+	ensure => present,
+	owner => irc,
+	group => irc,
+	mode => 0644,
+	source => "/home/egerlach/puppet/ngircd.conf",
+	notify => Service[ngircd]
+}
