b84af49f7dd9990ba2743fc9e9135cb453a6ccf6
Eric Gerlach <eric+git@gerlach.ca>
2012-03-03 13:38:21 -0500

    Clean up for deployment to a server

--- example1.pp
+++
@@ -1,5 +0,0 @@
-file { "/tmp/puppet/example1":
-	ensure => file,
-	mode => 0644,
-	content => "example1"
-}
--- site.pp
+++
@@ -1,5 +0,0 @@
-class { "ngircd": 
-	ircname => "irc1.awesome.net",
-	infotext => "Run with awesomeness"
-}
-class { "ngircd::no_motd": }
