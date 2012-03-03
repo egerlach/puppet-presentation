b425dcf17f3c76994198193f79f9fad99913288a
Eric Gerlach <eric+git@gerlach.ca>
2012-03-03 12:28:31 -0500

    Factor out the class into the module

---
+++ ngircd/manifests/init.pp
@@ -0,0 +1,30 @@
+class ngircd {
+	$motd = "Welcome to ${::fqdn}, my hell"
+
+	package { "ngircd":
+		provider => "apt",
+		ensure => present
+	}
+
+	service { "ngircd":
+		ensure => running,
+		hasrestart => true,
+		hasstatus => true,
+		require => Package[ngircd]
+	}
+
+	file { "/etc/ngircd/ngircd.conf":
+		ensure => present,
+		owner => irc,
+		group => irc,
+		mode => 0644,
+		source => "/home/egerlach/puppet/ngircd.conf",
+		notify => Service[ngircd]
+	}
+
+	file { "/etc/ngircd/ngircd.motd":
+		ensure => present,
+		content => $motd,
+		notify => Service[ngircd]
+	}
+}
